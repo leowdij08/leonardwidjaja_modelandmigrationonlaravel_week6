95038279dbed232cf49b85b9fe3b211cc0de9cf4
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2024-10-31 10:06:08 +0800

    upload

---
+++ test6/app/Models/Journals.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Journals extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'juduljurnal',
+        'namapenulis1',
+        'ringkasanjurnal',
+        'linkaksesjurnal',
+        'tahunterbit',
+    ];
+}
---
+++ test6/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WELCOME</title>
+    <style>
+        h1,h3{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello Leonard !!</h1>
+    <h3>Selamat datang di project pertama Laravel </h3>
+</body>
+</html>
